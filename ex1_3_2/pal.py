a7c0d99ee2c737282185c6fe6be21bb196f7cd8c
Konstantin Kolyagin <37573456+kkolyagin@users.noreply.github.com>
2022-09-25 14:01:17 +0300

    first add

---
+++ ex1_3_2/pal.py
@@ -0,0 +1,11 @@
+def palindrom(a):
+    palindrom = (a.replace(' ', '') ==a.replace(' ', '')[::-1])
+    return palindrom
+
+x = ('taco cat','rotator','black cat')
+
+for i in range(0,len(x)):
+    print (f"Пример {i+1}:")
+    print (f"Ввод: {x[i]}")
+    print (f"Вывод: {palindrom(x[i])}")
+    print()
