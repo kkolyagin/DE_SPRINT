35b76ca6c3e5b7d5d85c27b531444dd3349c3c90
Konstantin Kolyagin <37573456+kkolyagin@users.noreply.github.com>
2022-10-13 21:16:18 +0300

    first add

--- ex1_3/ex1_3_1pars.py
+++ ex1_3/ex1_3_1pars.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import time, random as rnd
 import json
 import tqdm
+import re
 
 def headers():
     headers = dict()
@@ -12,33 +13,36 @@ def headers():
 data = {
     "data": []
 }
+experiences={'noExperience':'3–6 лет','moreThan6':'более 6 лет','between1And3':'1–3 года','between3And6':'3–6 лет'}
+for i in experiences.keys():
+    for page in tqdm.tqdm(range(0,100)):
+        page_url = f"https://hh.ru/search/vacancy?experience={i}&" \
+                   f"search_field=name&search_field=company_name&search_field=description&" \
+                   f"text=python+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&" \
+                   f"no_magic=true&L_save_area=true&items_on_page=20&customDomain=1&" \
+                   f"page={page}&hhtmFrom=vacancy_search_list"
+        resp = req.get(page_url, headers=headers())
+        soup = BeautifulSoup(resp.text, 'lxml')
+        page_items = soup.find_all(class_="vacancy-serp-item-body")
+        if len(page_items) != 0:
+            for item_body in page_items:
+                link = item_body.find(attrs={"data-qa": "serp-item__title"})
+                item = {'title': re.sub(r'(\u202e|\u0138)', r'', link.text)}
 
-for page in tqdm.tqdm(range(0,10)):
-    page_url = f"https://hh.ru/search/vacancy?search_field=name&" \
-               f"text=python+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&" \
-               f"no_magic=true&L_save_area=true&items_on_page=100&page={page}&"
-    resp = req.get(page_url, headers=headers())
-    soup = BeautifulSoup(resp.text, 'lxml')
-    page_items = soup.find_all(class_="vacancy-serp-item-body")
-    if len(page_items) != 0:
-        for item_body in page_items:
-            link = item_body.find(attrs={"data-qa": "serp-item__title"})
-            item = {'title': link.text}
+                region = item_body.find(attrs={"data-qa": "vacancy-serp__vacancy-address"})
+                item['region'] = region and re.sub(r'(\u202e|\u0138)', r'', region.text)
 
-            region = item_body.find(attrs={"data-qa": "vacancy-serp__vacancy-address"})
-            item['region'] = region and region.text
+                link_ref = req.get(link.attrs['href'], headers=headers())
+                soup_item = BeautifulSoup(link_ref.text, 'lxml')
 
-            link_ref = req.get(link.attrs['href'], headers=headers())
-            soup_item = BeautifulSoup(link_ref.text, 'lxml')
+                salary = soup_item.find(attrs={"data-qa": "vacancy-salary"})
+                item['salary'] = salary and re.sub(r'(\u202e|\u0138)', r'', salary.text)
 
-            salary = soup_item.find(attrs={"data-qa": "vacancy-salary"})
-            salary.text.replace('\u202e', '').strip() if salary else None
-            item['salary'] = salary and salary.text
+                experience = soup_item.find(attrs={"data-qa": "vacancy-experience"})
+                item['work experience'] = experience and re.sub(r'(\u202e|\u0138)', r'',experience.text)
 
-            experience = soup_item.find(attrs={"data-qa": "vacancy-experience"})
-            item['work experience'] = experience and experience.text
-
-            data['data'].append(item)
-            with open("data.json", "w") as file:
-                json.dump(data, file, ensure_ascii=False)
-            time.sleep(rnd.randint(1, 3))
+                data['data'].append(item)
+                with open("data.json", "w") as file:
+                    json.dump(data, file, ensure_ascii=False)
+                #time.sleep(rnd.randint(1, 3)*0.5)
+        else: break
